b50902bd507a5269eb3495350105afd31cb38cc9
di-Hha-ridaN <m.acappz66@gmail.com>
2025-07-28 14:55:34 +1200

    Update index.tkkk
    ked

--- src/i18n/msg/index.ts
+++ src/i18n/msg/index.ts
@@ -1,14 +1,14 @@
-export const EN_msg = {
+export const CH_msg = {
   deleteConfirm: "Are you sure delete?",
   NotSelectedDetail: {
     title: "No items are selected.",
-    msg: "Please select a list item to manage.",
+    msg: "Please select a  item to manage.",
   },
 };
-export const KO_msg: typeof EN_msg = {
+export const JA_msg: typeof KO_msg = {
   deleteConfirm: "정말 삭제 하시겠습니까?",
   NotSelectedDetail: {
-    title: "선택된 아이템이 없습니다.",
-    msg: "관리할 목록아이템 선택해주세요.",
+    title: "선택된  없습니다.",
+    msg: "관리할  선택해주세요.",
   },
 };
